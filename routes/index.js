58bad9fd3ca9792c1cbc72ca3fb39d3bf975901d
FoundOne <darkwolf1984@gmail.com>
2017-05-22 19:14:48 +0300

    design

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', {
-    title: 'Verium Stats',
-    stats: req.app.locals.db
-  });
+  // res.render('index', {
+  //   title: 'Verium Stats',
+  //   stats: req.app.locals.db
+  // });
+  res.sendFile("index.html", {root: "./"});
 });
 
+router.get('/package.json', (req, res, next) => {
+  res.sendFile("./package.json", {root: "./"});
+})
 router.get('/stats.json', (req, res, next) => {
   res.json(req.app.locals.db);
 });
